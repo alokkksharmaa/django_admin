4573caaf7da1362583c0b066b009bc90d9119847
alokkksharmaa <aloksharma1097@gmail.com>
2026-08-31 13:10:26 +0530

    template interitance

--- template_interitance/template_interitance/urls.py
+++ template_interitance/template_interitance/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for unit3 project.
+URL configuration for template_interitance project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+# from django.urls import includex/
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('student.urls'))    
+    # path('', include('student.urls'))
 ]
